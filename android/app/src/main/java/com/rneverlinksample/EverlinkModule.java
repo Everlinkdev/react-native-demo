c297803b35bf4d1b06faa4ac0d66ab684d81dc04
Wen Ge <wenge8n@outlook.com>
2021-12-20 18:06:33 +0800

    fix

--- android/app/src/main/java/com/rneverlinksample/EverlinkModule.java
+++ android/app/src/main/java/com/rneverlinksample/EverlinkModule.java
@@ -1,11 +1,15 @@
 package com.rneverlinksample;
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 
+import com.everlink.broadcast.util.Everlink;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableArray;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -13,6 +17,9 @@ import java.util.Map;
 import java.util.HashMap;
 
 public class EverlinkModule extends ReactContextBaseJavaModule {
+
+    private Everlink EverLinkConnect;
+
     EverlinkModule(ReactApplicationContext context) {
         super(context);
     }
@@ -22,5 +29,80 @@ public class EverlinkModule extends ReactContextBaseJavaModule {
     @Override
     public String getName() {
         return "EverlinkModule";
+    }
+
+    @ReactMethod
+    public void initialize(String appIDKey) {
+        EverLinkConnect = new Everlink(getReactApplicationContext(), getCurrentActivity(), appIDKey);
+        EverLinkConnect.setAudioListener(new Everlink.audioListener() {
+
+            @Override
+            public void onAudioCodeReceived(String token) {
+                // you can now identify, via the server returned token, what location/device was heard
+                Log.d("newToken", token);
+            }
+
+            @Override
+            public void onEverLinkError(String error) {
+                //return the type of error received: server response, no internet, no permissions
+                Log.d("error", error);
+            }
+
+            @Override
+            public void onMyTokenGenerated(String oldToken, String newToken) {
+                //a new token generated, to save in your database
+                Log.d("newToken", newToken);
+                Log.d("oldToken", oldToken);
+            }
+        });
+    }
+
+    @ReactMethod
+    public void startListening(boolean isOffline) {
+        EverLinkConnect.startListening(isOffline);
+    }
+
+    @ReactMethod
+    public void stopListening() {
+        EverLinkConnect.stopListening();
+    }
+
+    @ReactMethod
+    public void playVolume(double volume, boolean useLoudspeaker) {
+        EverLinkConnect.playVolume(volume, useLoudspeaker);
+    }
+
+    @ReactMethod
+    public void startEmitting() {
+        EverLinkConnect.startEmitting();
+    }
+
+    @ReactMethod
+    public void stopEmitting() {
+        EverLinkConnect.stopEmitting();
+    }
+
+    @ReactMethod
+    public void startEmittingToken(String token, boolean isOffline) {
+        EverLinkConnect.startEmittingToken(token, isOffline);
+    }
+
+    @ReactMethod
+    public void createNewToken(String startDate) {
+        EverLinkConnect.createNewToken(startDate);
+    }
+
+    @ReactMethod
+    public void saveSounds(ReadableArray tokensArray) {
+        String[] tokens = new String[tokensArray.size()];
+        for (int i = 0; i < tokensArray.size(); i++) {
+            tokens[i] = tokensArray.getString(i);
+        }
+        EverLinkConnect.saveSounds(tokens);
+    }
+
+    @ReactMethod
+    public void clearSounds() {
+        EverLinkConnect.clearSounds();
     }
 }
--- android/app/src/main/java/com/rneverlinksample/MainActivity.java
+++ android/app/src/main/java/com/rneverlinksample/MainActivity.java
@@ -2,20 +2,15 @@ package com.rneverlinksample;
 
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import com.everlink.broadcast.util.Everlink;
 import com.facebook.react.ReactActivity;
-import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.ReadableArray;
 
 public class MainActivity extends ReactActivity {
 
   private static final int REQUEST_MICROPHONE = 8000;
-  private Everlink EverLinkConnect;
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
@@ -30,85 +25,11 @@ public class MainActivity extends ReactActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(null);
 
-    String myAppID = "upworkDemoKey11";
-
     //check permissions
     if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.RECORD_AUDIO)
             != PackageManager.PERMISSION_GRANTED ) {
       ActivityCompat.requestPermissions( this, new String[] {
               android.Manifest.permission.RECORD_AUDIO  }, REQUEST_MICROPHONE);
     }
-
-    EverLinkConnect = new Everlink(getApplicationContext(), this, myAppID);
-    EverLinkConnect.setAudioListener(new Everlink.audioListener() {
-
-      @Override
-      public void onAudioCodeReceived(String token) {
-        // you can now identify, via the server returned token, what location/device was heard
-        Log.d("newToken", token);
-      }
-
-      @Override
-      public void onEverLinkError(String error) {
-        //return the type of error received: server response, no internet, no permissions
-        Log.d("error", error);
-      }
-
-      @Override
-      public void onMyTokenGenerated(String oldToken, String newToken) {
-        //a new token generated, to save in your database
-        Log.d("newToken", newToken);
-        Log.d("oldToken", oldToken);
-      }
-    });
-  }
-
-  @ReactMethod
-  public void startListening(boolean isOffline) {
-    EverLinkConnect.startListening(isOffline);
-  }
-
-  @ReactMethod
-  public void stopListening() {
-    EverLinkConnect.stopListening();
-  }
-
-  @ReactMethod
-  public void playVolume(double volume, boolean useLoudspeaker) {
-    EverLinkConnect.playVolume(volume, useLoudspeaker);
-  }
-
-  @ReactMethod
-  public void startEmitting() {
-    EverLinkConnect.startEmitting();
-  }
-
-  @ReactMethod
-  public void stopEmitting() {
-    EverLinkConnect.stopEmitting();
-  }
-
-  @ReactMethod
-  public void startEmittingToken(String token, boolean isOffline) {
-    EverLinkConnect.startEmittingToken(token, isOffline);
-  }
-
-  @ReactMethod
-  public void createNewToken(String startDate) {
-    EverLinkConnect.createNewToken(startDate);
-  }
-
-  @ReactMethod
-  public void saveSounds(ReadableArray tokensArray) {
-    String[] tokens = new String[tokensArray.size()];
-    for (int i = 0; i < tokensArray.size(); i++) {
-      tokens[i] = tokensArray.getString(i);
-    }
-    EverLinkConnect.saveSounds(tokens);
-  }
-
-  @ReactMethod
-  public void clearSounds() {
-    EverLinkConnect.clearSounds();
   }
 }
