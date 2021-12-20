ce4ab0a1d46673695b4f47076d1c65b9515d0673
Wen Ge <wenge8n@outlook.com>
2021-12-20 11:52:01 +0800

    set up everlink sdk

--- android/app/src/main/java/com/rneverlinksample/MainActivity.java
+++ android/app/src/main/java/com/rneverlinksample/MainActivity.java
@@ -1,8 +1,19 @@
 package com.rneverlinksample;
 
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.everlink.broadcast.util.Everlink;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
+
+  private static final int REQUEST_MICROPHONE = 8000;
+  private Everlink EverLinkConnect;
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
@@ -11,5 +22,42 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "RNEverlinkSample";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+
+    String myAppID = "upworkDemoKey11";
+
+    //check permissions
+    if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.RECORD_AUDIO)
+            != PackageManager.PERMISSION_GRANTED ) {
+      ActivityCompat.requestPermissions( this, new String[] {
+              android.Manifest.permission.RECORD_AUDIO  }, REQUEST_MICROPHONE);
+    }
+
+    EverLinkConnect = new Everlink(getApplicationContext(), this, myAppID);
+    EverLinkConnect.setAudioListener(new Everlink.audioListener() {
+
+      @Override
+      public void onAudioCodeReceived(String token) {
+        // you can now identify, via the server returned token, what location/device was heard
+        Log.d("newToken", token);
+      }
+
+      @Override
+      public void onEverLinkError(String error) {
+        //return the type of error received: server response, no internet, no permissions
+        Log.d("error", error);
+      }
+
+      @Override
+      public void onMyTokenGenerated(String oldToken, String newToken) {
+        //a new token generated, to save in your database
+        Log.d("newToken", newToken);
+        Log.d("oldToken", oldToken);
+      }
+    });
   }
 }
