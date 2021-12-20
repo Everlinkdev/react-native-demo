a9c99eb54d95ea51176fc5f091663ba84c5fb7f7
Wen Ge <wenge8n@outlook.com>
2021-12-20 21:48:54 +0800

    update

--- App.js
+++ App.js
@@ -6,34 +6,57 @@
  * @flow strict-local
  */
 
-import React, {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
+  LogBox,
   NativeEventEmitter,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
+  TextInput,
   TouchableOpacity,
   useColorScheme,
   View,
 } from 'react-native';
+import CheckBox from '@react-native-community/checkbox';
+import DatePicker from 'react-native-date-picker';
+import moment from 'moment';
 
 import Everlink from './Everlink';
 
+LogBox.ignoreAllLogs(); 
+
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
+
+  const [audioCode, setAudioCode] = useState('');
+  const [everlinkError, setEverlinkError] = useState('');
+  const [myToken, setMyToken] = useState(null);
+
+  const [isOffline, setOffline] = useState(false);
+  const [volume, setVolume] = useState('0.5');
+  const [useLoudspeaker, setUseLoudspeaker] = useState(false);
+  const [token, setToken] = useState('');
+  const [isOffline2, setOffline2] = useState(false);
+  const [date, setDate] = useState(new Date());
+  const [isDatepickerVisible, setDatepickerVisible] = useState(false);
+  const [tokens, setTokens] = useState('');
 
   useEffect(() => {
     const eventEmitter = new NativeEventEmitter(Everlink);
     const onAudioCodeReceivedListener = eventEmitter.addListener('onAudioCodeReceived', (token) => {
       console.log(token);
+      setAudioCode(token);
     });
     const onEverLinkErrorListener = eventEmitter.addListener('onEverLinkError', (error) => {
       console.log(error);
+      setEverlinkError(error);
     });
     const onMyTokenGeneratedListener = eventEmitter.addListener('onMyTokenGenerated', (res) => {
       console.log(res);
+      setMyToken(res);
     });
 
     return () => {
@@ -43,27 +66,124 @@ const App = () => {
     }
   })
 
-  const startListening = (isOffline) => {
-    Everlink.startListening(isOffline);
-  }
-
-  const stopListening = () => {
-    Everlink.stopListening();
-  }
-
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: 'white'}}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView>
+      <ScrollView keyboardShouldPersistTaps="handled">
         <View style={styles.container}>
-          <TouchableOpacity style={styles.button} onPress={() => startListening(true)}>
+          <View style={{marginBottom: 16}}>
+            <View>
+              <Text style={{fontWeight: 'bold'}}>onAudioCodeReceived: </Text>
+              <Text style={{marginTop: 4}}>{audioCode}</Text>
+            </View>
+            <View>
+              <Text style={{fontWeight: 'bold'}}>onEverLinkError: </Text>
+              <Text style={{marginTop: 4}}>{everlinkError}</Text>
+            </View>
+            <View>
+              <Text style={{fontWeight: 'bold'}}>onMyTokenGenerated: </Text>
+              <Text style={{marginTop: 4}}>oldToken: {myToken?.oldToken}</Text>
+              <Text style={{marginTop: 4}}>newToken: {myToken?.newToken}</Text>
+            </View>
+          </View>
+
+          <View style={styles.divider} />
+
+          <View style={{flexDirection: 'row', alignItems: 'center'}}>
+            <CheckBox
+              value={isOffline}
+              onValueChange={(newValue) => setOffline(newValue)}
+            />
+            <Text>isOffline</Text>
+          </View>
+          <TouchableOpacity style={styles.button} onPress={() => Everlink.startListening(isOffline)}>
             <Text style={styles.buttonText}>startListening</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={stopListening}>
+          <TouchableOpacity style={styles.button} onPress={() => Everlink.stopListening()}>
             <Text style={styles.buttonText}>stopListening</Text>
+          </TouchableOpacity>
+
+          <View style={styles.divider} />
+
+          <View style={{flexDirection: 'row', alignItems: 'center', marginBottom: 16}}>
+            <View style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}>
+              <Text>Volume: </Text>
+              <TextInput style={{flex: 1, height: 40, borderWidth: 1}} value={volume} onChangeText={setVolume} keyboardType='numeric' />
+            </View>
+            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+              <CheckBox
+                value={useLoudspeaker}
+                onValueChange={(newValue) => setUseLoudspeaker(newValue)}
+              />
+              <Text>useLoudspeaker</Text>
+            </View>
+          </View>
+          <TouchableOpacity style={styles.button} onPress={() => Everlink.playVolume(volume, useLoudspeaker)}>
+            <Text style={styles.buttonText}>playVolume</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => Everlink.startEmitting()}>
+            <Text style={styles.buttonText}>startEmitting</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => Everlink.stopEmitting()}>
+            <Text style={styles.buttonText}>stopEmitting</Text>
+          </TouchableOpacity>
+          <View style={{flexDirection: 'row', alignItems: 'center', marginTop: 16, marginBottom: 16}}>
+            <View style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}>
+              <Text>Token: </Text>
+              <TextInput style={{flex: 1, height: 40, borderWidth: 1}} value={token} onChangeText={setToken} />
+            </View>
+            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+              <CheckBox
+                value={isOffline2}
+                onValueChange={(newValue) => setOffline2(newValue)}
+              />
+              <Text>isOffline</Text>
+            </View>
+          </View>
+          <TouchableOpacity style={styles.button} onPress={() => Everlink.startEmittingToken(token, isOffline2)}>
+            <Text style={styles.buttonText}>startEmittingToken</Text>
+          </TouchableOpacity>
+
+          <View style={styles.divider} />
+
+          <TouchableOpacity style={{marginBottom: 8}} onPress={() => setDatepickerVisible(true)}>
+            <Text>{moment(date).format('YYYY-MM-DD')}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => Everlink.createNewToken(moment(date).format('YYYY-MM-DD'))}>
+            <Text style={styles.buttonText}>createNewToken</Text>
+          </TouchableOpacity>
+
+          <View style={styles.divider} />
+
+          <TextInput
+            style={{height: 120, borderWidth: 1, marginBottom: 12}}
+            value={tokens}
+            onChangeText={setTokens}
+            multiline
+            textAlignVertical="top"
+            placeholder={`exampleToken1\nexampleToken2\nexampleToken3`} />
+          <TouchableOpacity style={styles.button} onPress={() => Everlink.saveSounds(tokens.split(/\r?\n/))}>
+            <Text style={styles.buttonText}>saveSounds</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => Everlink.clearSounds()}>
+            <Text style={styles.buttonText}>clearSounds</Text>
           </TouchableOpacity>
         </View>
       </ScrollView>
+
+      <DatePicker
+        modal
+        open={isDatepickerVisible}
+        date={date}
+        onConfirm={(date) => {
+          setDatepickerVisible(false);
+          setDate(date);
+        }}
+        onCancel={() => {
+          setDatepickerVisible(false);
+        }}
+        mode="date"
+      />
     </SafeAreaView>
   );
 };
@@ -85,6 +205,12 @@ const styles = StyleSheet.create({
   buttonText: {
     color: 'black',
   },
+  divider: {
+    backgroundColor: 'lightgray',
+    width: '100%',
+    height: 1,
+    marginBottom: 16,
+  }
 });
 
 export default App;
