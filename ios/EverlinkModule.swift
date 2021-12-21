9aa0d258f33d173e807eb3db0ea1018efcf4572a
Wen Ge <wenge8n@outlook.com>
2021-12-21 22:27:02 +0800

    implement methods

--- ios/EverlinkModule.swift
+++ ios/EverlinkModule.swift
@@ -6,9 +6,12 @@
 //
 
 import Foundation
+import EverlinkBroadcastSDK
 
 @objc(EverlinkModule)
-class EverlinkModule: RCTEventEmitter {
+class EverlinkModule: RCTEventEmitter, EverlinkConnectDelegate {
+  
+  var connectObj:Everlink?
   
   override func supportedEvents() -> [String]! {
     return ["onAudioCodeReceived", "onEverLinkError", "onMyTokenGenerated"]
@@ -19,33 +22,55 @@ class EverlinkModule: RCTEventEmitter {
   }
   
   @objc func initialize(_ appIDKey: String) -> Void {
+    connectObj = Everlink(appID: appIDKey)
+    connectObj?.delegate = self
+  }
+  
+  func onAudioCodeReceived(token: String) {
+    print(token)
+    sendEvent(withName: "onAudioCodeReceived", body: token)
+  }
+  
+  func onMyTokenGenerated(token: String, oldToken: String) {
+    print("New token: " + token)
+    print("Old token: " + oldToken)
+    sendEvent(withName: "onMyTokenGenerated", body: ["oldToken": oldToken, "newToken": token])
   }
   
   @objc func startListening(_ isOffline: Bool) -> Void {
+    connectObj?.startRecording(isOffline: isOffline)
   }
   
   @objc func stopListening() -> Void {
+    connectObj?.stopRecording()
   }
   
   @objc func playVolume(_ volume: Double, useLoudspeaker: Bool) -> Void {
+    connectObj?.playVolume(volume: Float(volume), loudspeaker: useLoudspeaker)
   }
   
   @objc func startEmitting() -> Void {
+    connectObj?.startEmitting()
   }
   
   @objc func stopEmitting() -> Void {
+    connectObj?.stopEmitting()
   }
   
   @objc func startEmittingToken(_ token: String, isOffline: Bool) -> Void {
+    connectObj?.startEmittingToken(token: token, isOffline: isOffline)
   }
   
   @objc func createNewToken(_ startDate: String) -> Void {
+    connectObj?.newToken(startDate: startDate)
   }
   
   @objc func saveSounds(_ tokensArray: [String]) -> Void {
+    connectObj?.saveSounds(tokensArray: tokensArray)
   }
   
   @objc func clearSounds() -> Void {
+    connectObj?.clearSounds()
   }
   
 }
